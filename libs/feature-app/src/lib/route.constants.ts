8cca850748737bc6120c03a1bbbf326880c83770
Pavel Vondra <pavelvondra66@gmail.com>
2024-02-24 11:40:38 +0100

    feat(feature-app): Add form routes

--- libs/feature-app/src/lib/route.constants.ts
+++ libs/feature-app/src/lib/route.constants.ts
@@ -1,8 +1,14 @@
-export const ROUTE_APP = {
+export const ROUTES_NAVBAR = {
   HOME_PAGE: { name: 'home', path: '/' },
   SEARCH_PAGE: { name: 'search', path: '/search' },
   FAVOURITES_PAGE: { name: 'favourites', path: '/favourites' },
   MEAL_PLAN_PAGE: { name: 'mealPlan', path: '/mealPlan' },
+};
+
+export const ROUTES_APP = {
+  ...ROUTES_NAVBAR,
+  SIGNIN_PAGE: { name: 'signin', path: '/signin' },
+  SIGNUP_PAGE: { name: 'signup', path: '/signup' },
 };
 
 export default {};
