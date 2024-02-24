ce52f17cbe75a80c08decca70380bf804174534c
Pavel Vondra <pavelvondra66@gmail.com>
2024-02-24 10:07:30 +0100

    feat(app-ui): Create app library for ui

---
+++ libs/app-ui/src/lib/app-ui.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import AppUi from './app-ui';
+
+describe('AppUi', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<AppUi />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/app-ui/src/lib/app-ui.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface AppUiProps {}
+
+export function AppUi(props: AppUiProps) {
+  return (
+    <div>
+      <h1>Welcome to AppUi!</h1>
+    </div>
+  );
+}
+
+export default AppUi;
