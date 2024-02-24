97c09106e3119a63a31ddbf15abe3a2073177ef7
Pavel Vondra <pavelvondra66@gmail.com>
2024-02-24 15:54:50 +0100

    feat: Move utils

--- libs/util-shared/src/index.ts
+++ libs/util-shared/src/index.ts
@@ -2,5 +2,6 @@ export * from './lib/shared-utils';
 export * from './lib/helpers/firebase.helpers';
 export * from './lib/helpers/storybook.helpers.tsx';
 export * from './lib/helpers/testing.helpers.tsx';
+export * from './lib/firebase.utils/firebase.utils';
 export { default as showToast } from './lib/helpers/toast.helpers';
 export { default as useScrollTop } from './lib/hooks/useScrollTop';
--- libs/util-shared/src/lib/firebase.utils.ts
+++ libs/util-shared/src/lib/firebase.utils.ts
@@ -1,6 +1,6 @@
-import Recipe from "../types/recipe.types";
+import { IRecipe } from 'util-types';
 
-export /**
+/**
  *
  * Checks if the data are object from RTL firebase or response from the API, then it processes.
  *
@@ -8,11 +8,11 @@ export /**
  * @return {*}  {Array<object>} -> returns set of objects as array or empty array, because if the favourite_recipes property
  * in RTL firebase database is without children, it is deleted, but it is managed in the CRUD.ts and the new value favourite_recipes: 0 is made
  */
-const recipeRowOrigin = (data: Recipe[]): Recipe[] => {
+export const recipeRowOrigin = (data: IRecipe[]): IRecipe[] => {
   if (!data) {
     return [];
   }
-  if (typeof data === "object") {
+  if (typeof data === 'object') {
     const arrayOfData = [...Object.values(data)].map((value) => {
       return value;
     });
