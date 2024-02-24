413f4c8aae3aa14227921e2f9eca484d103d6990
Pavel Vondra <pavelvondra66@gmail.com>
2024-02-24 12:09:49 +0100

    feat(firebase): Add node starting lib

---
+++ libs/data-access-firebase/src/lib/data-access-firebase.spec.ts
@@ -0,0 +1,7 @@
+import { dataAccessFirebase } from './data-access-firebase';
+
+describe('dataAccessFirebase', () => {
+  it('should work', () => {
+    expect(dataAccessFirebase()).toEqual('data-access-firebase');
+  });
+});
---
+++ libs/data-access-firebase/src/lib/data-access-firebase.ts
@@ -0,0 +1,3 @@
+export function dataAccessFirebase(): string {
+  return 'data-access-firebase';
+}
